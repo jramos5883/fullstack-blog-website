4192c0953e4e2bd0caab8cc554256e4e97c7458c
jramos5883 <jramos5883@gmail.com>
2023-10-06 17:19:22 -0700

    Finished Navbar

--- fsbw-app/app/layout.js
+++ fsbw-app/app/layout.js
@@ -1,17 +1,21 @@
 import "./globals.css";
+import Navbar from "./navbar";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "{Object}",
+  title: "{My Blog Website}",
   description: "Full Stack Blog Website",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body id="root" className={inter.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ fsbw-app/app/navbar.jsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "@mui/material";
+import { GiFlamingTrident } from "react-icons/gi";
+import { Typography } from "@mui/material/";
+import Avatar from "@mui/material/Avatar";
+
+export default function Navbar() {
+  "https://static.vecteezy.com/system/resources/thumbnails/003/337/584/small/default-avatar-photo-placeholder-profile-icon-vector.jpg";
+  return (
+    <nav className="flex flex-row items-center justify-between h-20 bg-sky-800">
+      <div className="flex flex-row items-center">
+        <div className="flex flex-row items-center pl-4">
+          <Link className="" href="/">
+            <span className="text-5xl text-cyan-400">
+              <GiFlamingTrident />
+            </span>
+          </Link>
+          <Link className="no-underline" href="/">
+            <Typography className="text-2xl text-bold pl-4 text-cyan-400">
+              Blog Website
+            </Typography>
+          </Link>
+        </div>
+      </div>
+      <div className="flex flex-row items-center pr-4">
+        <Button className="mr-4" variant="contained">
+          SIGN IN
+        </Button>
+        {/* <Image
+          className="rounded-full"
+          src={USER_IMAGE}
+          width={60}
+          height={60}
+          alt=""
+        /> */}
+        <Avatar alt="J User Avatar" src="" />
+      </div>
+    </nav>
+  );
+}
